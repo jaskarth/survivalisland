26a35a1dfbb7fa42dbbc74942ee6892c267cd878
SuperCoder79 <25208576+SuperCoder7979@users.noreply.github.com>
2022-08-07 10:09:09 -0400

    Port to 1.19 by removing every bit of content

--- src/main/java/supercoder79/survivalisland/SurvivalIsland.java
+++ src/main/java/supercoder79/survivalisland/SurvivalIsland.java
@@ -7,20 +7,12 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import supercoder79.survivalisland.config.ConfigData;
 import supercoder79.survivalisland.config.ConfigSerializer;
-import supercoder79.survivalisland.world.IslandBiomeSource;
-import supercoder79.survivalisland.world.IslandWorldType;
 
 public class SurvivalIsland implements ModInitializer {
 	public static ConfigData CONFIG;
-	private static IslandWorldType worldType;
 
 	@Override
 	public void onInitialize() {
 		CONFIG = ConfigSerializer.init();
-		if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
-			worldType = new IslandWorldType();
-		}
-
-		Registry.register(Registry.BIOME_SOURCE, new Identifier("survivalisland", "island"), IslandBiomeSource.CODEC);
 	}
 }
